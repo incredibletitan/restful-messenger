9eb77fed3251ae62bff522a7d043aa0d61c7e12a
Pretender <100sbsh@gmail.com>
2015-12-08 07:27:36 +0200

    Added basic routes

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/', function () use ($app) {
-    return 'hello';
+// accept JSON
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
 });
+
+$app->get('/messages/', function() use ($app) {
+    return 'get messages';
+});
+
+$app->get('/messages/{id}', function(Silex\Application $app, $id) use ($app) {
+    return 'messages ' . $id;
+});
+
+$app->post('/messages/', function() use ($app) {
+	return 'messages post';
+});
+
+
 
 $app->run();
